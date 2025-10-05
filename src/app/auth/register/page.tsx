cdf1b2c6f2e8643087febec49f59d311ccdaaaab
mmgolebi <maciekmg12@gmail.com>
2025-10-05 10:58:42 -0400

    Fix syntax error in register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -138,7 +138,7 @@ export default function RegisterPage() {
                   type="button"
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 p-2"
-                />
+                >
                   {showConfirmPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
                 </button>
               </div>
