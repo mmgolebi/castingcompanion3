3e84d8eedf30a442500c168100749bc03e2ad262
mmgolebi <maciekmg12@gmail.com>
2025-10-05 21:04:37 -0400

    Increase file upload limit to 20MB

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+  imageUploader: f({ image: { maxFileSize: "20MB", maxFileCount: 1 } })
     .middleware(async () => {
       return {};
     })
@@ -12,7 +12,7 @@ export const ourFileRouter = {
       return { url: file.url };
     }),
   
-  pdfUploader: f({ pdf: { maxFileSize: "4MB", maxFileCount: 1 } })
+  pdfUploader: f({ pdf: { maxFileSize: "20MB", maxFileCount: 1 } })
     .middleware(async () => {
       return {};
     })
