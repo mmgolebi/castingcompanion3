3737d4c0d045c8747c449ba96320507ce609d60d
mmgolebi <maciekmg12@gmail.com>
2025-10-05 20:58:49 -0400

    Remove generic type from UploadButton wrapper

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -4,5 +4,5 @@ import { UploadButton as UTButton } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export function UploadButton(props: any) {
-  return <UTButton<OurFileRouter> {...props} />;
+  return <UTButton {...props} />;
 }
